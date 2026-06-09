9b67518c5bc97e0a06b3e7e0bd9c4f83a3ab2ef2
Prince Tiwari <tprince0634@gmail.com>
2026-06-09 12:04:59 +0530

    Changes to constructor

--- EMS/Employee.java
+++ EMS/Employee.java
@@ -1,8 +1,10 @@
 class Employee{
+	Employee(){
+		System.out.println("Welcome to The Employee Management System");	
+		System.out.println("Welcome to The Employee Management System");	
+		}
+		
 	public static void main(String [] args){
-		
-		
-		Employee(){
-			System.out.println("Welcome to The Employee Management System");	
+		Employee emp = new  Employee();
 	}
 }
